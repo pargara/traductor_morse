22a367306a157f7de2503b0bdf90051b38c6301b
Sebastian Guevara <sebastian.guevarapa@gmail.com>
2022-11-14 21:44:57 -0500

    Se agrego el espacio

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-codigoM = { 'A':'.-', 'B':'-...',
-                    'C':'-.-.', 'D':'-..', 'E':'.',
-                    'F':'..-.', 'G':'--.', 'H':'....',
-                    'I':'..', 'J':'.---', 'K':'-.-',
-                    'L':'.-..', 'M':'--', 'N':'-.',
-                    'O':'---', 'P':'.--.', 'Q':'--.-',
-                    'R':'.-.', 'S':'...', 'T':'-',
-                    'U':'..-', 'V':'...-', 'W':'.--',
-                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
-                    '.':'.-.-.-',',':'-.-.--',' ':'/'}
+codigoM = { "A":".-", "B":"-...",
+                    "C":"-.-.", "D":"-..", "E":".",
+                    "F":"..-.", "G":"--.", "H":"....",
+                    "I":"..", "J":".---", "K":"-.-",
+                    "L":".-..", "M":"--", "N":"-.",
+                    "O":"---", "P":".--.", "Q":"--.-",
+                    "R":".-.", "S":"...", "T":"-",
+                    "U":"..-", "V":"...-", "W":".--",
+                    "X":"-..-", "Y":"-.--", "Z":"--..",
+                    ".":".-.-.-",",":"-.-.--"," ":"/"}
 def encriptar(mensaje):
-    cifrar = ''
+    cifrar = ""
     for letra in mensaje:
-        if letra != ' ':
-            cifrar += codigoM[letra] + ' '
+        if letra != "+":
+            cifrar += codigoM[letra] + " "
         else:
             cifrar += ' '
     return cifrar
 
-mensaje = "Hey dude"
+print(f"3 \n Hola mundo \n La mariposa revolotea, como si desesperara en este mundo \n Este es un mensaje programado en Python, pero codificado en morse")
+
+mensaje = "Hola mundo"
 resultado = encriptar(mensaje.upper())
 print(resultado)
+print(f"\n")
+
+mensaje = "La mariposa revolotea, como si desesperara en este mundo."
+resultado = encriptar(mensaje.upper())
+print(resultado)
+print(f"\n")
+
+mensaje = "Este es un mensaje programado en Python, pero codificado en morse."
+resultado = encriptar(mensaje.upper())
+print(resultado)
+print("\n")
